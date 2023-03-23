ddeef342ca483d004b86827724de02d6f27ab25e
arya3d <aria.salehi.dev@gmail.com>
2023-03-23 09:39:39 +0330

    cleanup

--- src/app.js
+++ src/app.js
@@ -13,62 +13,40 @@ import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
 import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
 import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
 
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
 const startApp = () => {
   const scene = useScene()
   const camera = useCamera()
   const gui = useGui()
-  const { width, height } = useRenderSize()
-
-  const ROTATION_SPEED = 0.02
-  const MOTION_BLUR_AMOUNT = 0.725
 
   const dirLight = new THREE.DirectionalLight('#ffffff', 1)
   const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
   scene.add(dirLight, ambientLight)
 
-  const geometry = new THREE.IcosahedronGeometry()
-  const material = new THREE.MeshStandardMaterial({
-    color: '#4e62f9',
+
+
+
+
+  const geometry = new THREE.PlaneGeometry()
+  const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader
   })
   const ico = new THREE.Mesh(geometry, material)
   scene.add(ico)
+  
+
+
+
 
   // GUI
   const cameraFolder = gui.addFolder('Camera')
   cameraFolder.add(camera.position, 'z', 0, 10)
   cameraFolder.open()
 
-  // postprocessing
-  const renderTargetParameters = {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.LinearFilter,
-    stencilBuffer: false,
-  }
-
-  // save pass
-  const savePass = new SavePass(new THREE.WebGLRenderTarget(width, height, renderTargetParameters))
-
-  // blend pass
-  const blendPass = new ShaderPass(BlendShader, 'tDiffuse1')
-  blendPass.uniforms['tDiffuse2'].value = savePass.renderTarget.texture
-  blendPass.uniforms['mixRatio'].value = MOTION_BLUR_AMOUNT
-
-  // output pass
-  const outputPass = new ShaderPass(CopyShader)
-  outputPass.renderToScreen = true
-
-  // adding passes to composer
-  addPass(blendPass)
-  addPass(savePass)
-  addPass(outputPass)
-
-  const animateIco = () => {
-    ico.rotation.x += ROTATION_SPEED
-    ico.rotation.y += ROTATION_SPEED
-  }
-
   useTick(({ timestamp, timeDiff }) => {
-    animateIco()
   })
 }
 
---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,14 @@
+varying vec2 vUv;
+
+void main() {
+    gl_FragColor = vec4(vUv.xxx, 1);
+}
+
+
+
+
+
+
+
+
+
